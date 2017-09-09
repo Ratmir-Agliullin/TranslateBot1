ca5df09b1ed2b4c74b1c822f6e5e449268d81788
Аглиуллины <ratmir-91@mail.ru>
2017-09-09 09:43:40 +0400

    jar solved exceptions,changed titles of keys, commands start and help

--- src/main/java/BotManager.java
+++ src/main/java/BotManager.java
@@ -189,8 +189,7 @@ private static String    keyString = "Hello! \n" +
             }
             else
                 if (message.getText().equals("/start"))
-
-                {  if(keyFlag!=0)
+                {  if((keyFlag!=0)&&(flag==0))
               //      keyFlag = 0; else
                     SendText(chatId, "Hello! \n"+
                             "If you want to add new eng and rus words press Add new eng-rus and firstly write eng word, secondly write rus word \n" +
@@ -206,7 +205,7 @@ private static String    keyString = "Hello! \n" +
 //                            "If you want get Rus word form database, press get Rus word and put word IN ENGLISH \n" +
 //                            "If you want get Rus word form database, press get Eng word and put word IN Russian \n" +
 //                            "Also You can use yandex translate: form rus to eng and from eng to rus";
-                flag=0;
+            //    flag=0;
 
             }else
                 if(message.getText().equals("/help")) {
